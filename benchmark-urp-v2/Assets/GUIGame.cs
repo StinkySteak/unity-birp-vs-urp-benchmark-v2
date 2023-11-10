7c1faeabd7b50273d29f16ac967f51f8bd200a54
StinkySteak <slimesteve17@gmail.com>
2023-11-10 21:51:10 +0700

    urp feat: added ui to change quality settings in runtime script

---
+++ benchmark-urp-v2/Assets/GUIGame.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class GUIGame : MonoBehaviour
+{
+    [SerializeField] private Button _buttonVeryLow;
+    [SerializeField] private Button _buttonUltra;
+
+    private void Start()
+    {
+        _buttonVeryLow.onClick.AddListener(OnButtonVeryLow);
+        _buttonUltra.onClick.AddListener(OnButtonUltra);
+    }
+
+    private void OnButtonVeryLow()
+        => QualitySettings.SetQualityLevel(0);
+    private void OnButtonUltra()
+        => QualitySettings.SetQualityLevel(1);
+}
